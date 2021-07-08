73b028a9e707c7db362d7f6268f922dd0e8c4a1d
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-07-08 16:13:12 +0900

    Feat: split secure file to other config file

---
+++ src/main/java/gg/adofai/server/DatabaseConfig.java
@@ -0,0 +1,47 @@
+package gg.adofai.server;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class DatabaseConfig {
+
+    private final GlobalPropertySource globalPropertySource;
+
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url(globalPropertySource.getUrl())
+                .driverClassName(globalPropertySource.getDriverClassName())
+                .username(globalPropertySource.getUsername())
+                .password(globalPropertySource.getPassword())
+                .build();
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource customDataSource) throws Exception {
+        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(customDataSource);
+        //PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        //sessionFactory.setMapperLocations(resolver.getResources("classpath:mybatis/mapper/*.xml"));
+        return sessionFactory.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+
+
+}
--- src/main/java/gg/adofai/server/ServerApplication.java
+++ src/main/java/gg/adofai/server/ServerApplication.java
@@ -1,5 +1,6 @@
 package gg.adofai.server;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
