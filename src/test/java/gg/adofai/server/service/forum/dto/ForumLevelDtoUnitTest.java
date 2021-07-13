1975f6cc412b2813dd93ad3fcc5f1d63f17faeb9
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-07-13 18:07:06 +0900

    Test: Write test code to ForumLevelDtoUnitTest

--- src/test/java/gg/adofai/server/service/forum/dto/ForumLevelDtoUnitTest.java
+++ src/test/java/gg/adofai/server/service/forum/dto/ForumLevelDtoUnitTest.java
@@ -5,16 +5,16 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class ForumLevelDtoTest {
-
-    private String jsonStr_ns = "[{\"v\":1,\"f\":\"1\"},{\"v\":\"[ns]\"},{\"v\":\"かめりあ(Camellia)\"},{\"v\":20,\"f\":\"20\"},{\"v\":\"Ruren\"},{\"v\":\"Link\"},{\"v\":\"X\"},{\"v\":\"Youtube\"},{\"v\":\"X\"},null,{\"v\":2542,\"f\":\"2542\"},{\"v\":\"#동시치기\"},{\"v\":\"#셋잇단\"},{\"v\":\"#개박\"},{\"v\":\"#4분이상\"},null,{\"v\":20,\"f\":\"20\"},null,null,null,null,{\"v\":null}]";
-    private String jsonStr_no28Empty = "[null,null,null,{\"v\":-2,\"f\":\"-2\"},null,null,{\"v\":\"X\"},null,null,null,null,null,null,null,null,null,{\"v\":-2,\"f\":\"-2\"},null,null,null,null,{\"v\":null}]";
+class ForumLevelDtoUnitTest {
 
     @Test
     void testNormalConvert() throws Exception {
         // given
+        String jsonStr_ns = "[{\"v\":1,\"f\":\"1\"},{\"v\":\"[ns]\"},{\"v\":\"かめりあ(Camellia)\"},{\"v\":20,\"f\":\"20\"},{\"v\":\"Ruren\"},{\"v\":\"Link\"},{\"v\":\"X\"},{\"v\":\"Youtube\"},{\"v\":\"X\"},null,{\"v\":2542,\"f\":\"2542\"},{\"v\":\"#동시치기\"},{\"v\":\"#셋잇단\"},{\"v\":\"#개박\"},{\"v\":\"#4분이상\"},null,{\"v\":20,\"f\":\"20\"},null,null,null,null,{\"v\":null}]";
         JSONArray arr = toJsonArray(jsonStr_ns);
 
         // when
@@ -22,21 +22,32 @@ class ForumLevelDtoTest {
 
         // then
         assertNotNull(dto);
-        assertEquals(dto.getId(), 1);
-
-
-
+        assertEquals(1, dto.getId());
+        assertEquals("[ns]", dto.getSong());
+        assertIterableEquals(List.of("かめりあ(Camellia)"), dto.getArtists());
+        assertEquals(20.0, dto.getLevel());
+        assertIterableEquals(List.of("Ruren"), dto.getCreators());
+        assertNull(dto.getDownload());
+        assertNull(dto.getWorkshop());
+        assertNull(dto.getVideo());
+        assertEquals(false, dto.getEpilepsyWarning());
+        assertNull(dto.getMinBpm());
+        assertNull(dto.getMaxBpm());
+        assertEquals(2542, dto.getTiles());
+        assertIterableEquals(List.of("동시치기", "셋잇단", "개박", "4분이상"), dto.getTags());
     }
 
     @Test
     void convertToNullTest() throws Exception {
         // given
-        
+        String jsonStr_no28Empty = "[null,null,null,{\"v\":-2,\"f\":\"-2\"},null,null,{\"v\":\"X\"},null,null,null,null,null,null,null,null,null,{\"v\":-2,\"f\":\"-2\"},null,null,null,null,{\"v\":null}]";
+        JSONArray arr = toJsonArray(jsonStr_no28Empty);
 
         // when
+        ForumLevelDto dto = ForumLevelDto.createForumLevelDto(arr);
 
         // then
-
+        assertNull(dto);
     }
 
     JSONArray toJsonArray(String str) throws ParseException {
