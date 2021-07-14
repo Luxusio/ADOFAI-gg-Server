6d4469651d7616117885e6fe5ee0aa2151b0fe65
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-07-14 21:06:15 +0900

    Test: Change constructor arguments in ForumServiceTest

--- src/test/java/gg/adofai/server/service/forum/ForumServiceTest.java
+++ src/test/java/gg/adofai/server/service/forum/ForumServiceTest.java
@@ -1,10 +1,8 @@
 package gg.adofai.server.service.forum;
 
 import gg.adofai.server.service.forum.dto.ForumTagDto;
-import manifold.ext.rt.api.Jailbreak;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.util.List;
@@ -16,7 +14,8 @@ class ForumServiceTest {
     @Test
     void testGetData() throws IOException {
         // given
-        @Jailbreak ForumService forumService = new ForumService(WebClient.builder());
+        ForumService forumService = new ForumService(
+                null, null, null, null, null, WebClient.builder());
 
         // when
         List<ForumTagDto> forumTagDtoList = forumService.getData(ForumService.GID_ADMIN_TAG, ForumTagDto::createForumTagDto).block();
@@ -25,5 +24,7 @@ class ForumServiceTest {
         assertNotNull(forumTagDtoList);
         assertNotEquals(0, forumTagDtoList.size());
     }
+
+
 
 }
