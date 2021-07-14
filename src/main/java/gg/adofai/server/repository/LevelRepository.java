5c4b30f1186c0dfed8289e1f5f7e990b860d90db
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-07-14 16:39:46 +0900

    Feat: Create Level, Member, Person, Song Repository

---
+++ src/main/java/gg/adofai/server/repository/LevelRepository.java
@@ -0,0 +1,41 @@
+package gg.adofai.server.repository;
+
+import gg.adofai.server.domain.entity.level.Level;
+import gg.adofai.server.domain.entity.level.Song;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class LevelRepository {
+
+    private final EntityManager em;
+
+    public void save(Level level) {
+        em.persist(level);
+    }
+
+    public Level findOne(Long id) {
+        return em.find(Level.class, id);
+    }
+
+    public List<Level> findAll() {
+        return em.createQuery("select l from Level l", Level.class)
+                .getResultList();
+    }
+
+    public Level findByTitle(String title) {
+        List<Level> result = findByTitles(List.of(title));
+        return result.isEmpty() ? null : result.get(0);
+    }
+
+    public List<Level> findByTitles(List<String> titles) {
+        return em.createQuery("select l from Level l where l.title = :titles", Level.class)
+                .setParameter("titles", titles)
+                .getResultList();
+    }
+
+}
