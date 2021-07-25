df9716198cecbdae3a2b7fd402125cbae7597cf8
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-07-26 03:30:10 +0900

    Test: Test LevelQueryRepository in various ways

--- src/test/java/gg/adofai/server/repository/level/LevelQueryRepositoryTest.java
+++ src/test/java/gg/adofai/server/repository/level/LevelQueryRepositoryTest.java
@@ -93,20 +93,116 @@ class LevelQueryRepositoryTest {
 
     }
 
-
-    @Test
-    void testSearchData() {
-
+    private LevelSearchCondition createBaseSearchCondition() {
         LevelSearchCondition condition = new LevelSearchCondition();
         condition.setOffset(0L);
         condition.setAmount(50L);
+        return condition;
+    }
+
+    @Test
+    void testSearchData() {
+        LevelSearchCondition condition = createBaseSearchCondition();
+        condition.setTags(List.of("동시치기"));
 
         LevelSearchResultDto resultDto = levelQueryRepository.searchLevel(condition);
 
-        System.out.println("resultDto = " + resultDto);
+        System.out.println("resultDto.count = " + resultDto.getCount());
         for (LevelDto result : resultDto.getResults()) {
             System.out.println("result = " + result);
         }
     }
+
+    @Test
+    void testSearchByTag() {
+        LevelSearchCondition condition = createBaseSearchCondition();
+        condition.setTags(List.of("동시치기", "셋잇단"));
+
+        LevelSearchResultDto resultDto = levelQueryRepository.searchLevel(condition);
+
+        assertTrue(resultDto.getCount() > 0, "Tag result should exist");
+    }
+
+    @Test
+    void testSearchByQuery() {
+        LevelSearchCondition condition = createBaseSearchCondition();
+        condition.setQuery("Camellia");
+        LevelSearchCondition condition2 = createBaseSearchCondition();
+        condition2.setQuery("secret boss");
+        LevelSearchCondition condition3 = createBaseSearchCondition();
+        condition3.setQuery("bWen");
+
+        LevelSearchResultDto resultDto = levelQueryRepository.searchLevel(condition);
+        LevelSearchResultDto resultDto2 = levelQueryRepository.searchLevel(condition2);
+        LevelSearchResultDto resultDto3 = levelQueryRepository.searchLevel(condition3);
+
+        assertTrue(resultDto.getCount() > 0, "camellia artist should exist");
+        assertTrue(resultDto2.getCount() > 0, "secret boss level name should exist");
+        resultDto2.getResults().forEach(levelDto -> assertTrue(levelDto.getTitle().toLowerCase().contains("secret boss"),
+                "level name should ONLY contain secret boss"));
+        assertTrue(resultDto3.getCount() > 0, "creator bWen should exists");
+    }
+
+    @Test
+    void testSearchByLevel() {
+        LevelSearchCondition condition = createBaseSearchCondition();
+        condition.setMinDifficulty(21.0);
+        LevelSearchCondition condition2 = createBaseSearchCondition();
+        condition2.setMaxDifficulty(1.0);
+
+        LevelSearchResultDto resultDto = levelQueryRepository.searchLevel(condition);
+        LevelSearchResultDto resultDto2 = levelQueryRepository.searchLevel(condition2);
+
+        assertNotEquals(0, resultDto.getCount());
+        resultDto.getResults().forEach(levelDto -> assertEquals(21, levelDto.getDifficulty(),
+                "difficulty should be only 21"));
+
+        assertNotEquals(0, resultDto2.getCount());
+        resultDto2.getResults().forEach(levelDto -> assertEquals(1.0, levelDto.getDifficulty(),
+                "difficulty should be only 1.0"));
+    }
+
+    @Test
+    void testSearchByOffsetAmount() {
+        LevelSearchCondition condition = createBaseSearchCondition();
+        condition.setOffset(50L);
+        condition.setAmount(50L);
+
+        LevelSearchResultDto resultDto = levelQueryRepository.searchLevel(condition);
+        assertNotEquals(0, resultDto.getCount());
+        assertEquals(50, resultDto.getResults().size());
+    }
+
+    @Test
+    void testSearchByBpm() {
+        LevelSearchCondition condition = createBaseSearchCondition();
+        condition.setMaxBpm(200.0);
+        LevelSearchCondition condition2 = createBaseSearchCondition();
+        condition2.setMinBpm(200.0);
+        LevelSearchCondition condition3 = createBaseSearchCondition();
+        condition3.setMinBpm(200.0);
+        condition3.setMaxBpm(200.0);
+
+        LevelSearchResultDto resultDto = levelQueryRepository.searchLevel(condition);
+        LevelSearchResultDto resultDto2 = levelQueryRepository.searchLevel(condition2);
+        LevelSearchResultDto resultDto3 = levelQueryRepository.searchLevel(condition3);
+
+        assertNotEquals(0, resultDto.getCount());
+        resultDto.getResults().forEach(levelDto -> assertTrue(200 >= levelDto.getMaxBpm(),
+                "max bpm should be equal or lower than 200"));
+
+        assertNotEquals(0, resultDto2.getCount());
+        resultDto2.getResults().forEach(levelDto -> assertTrue(200 <= levelDto.getMinBpm(),
+                "min bpm should be equal or higher than 200"));
+
+        assertNotEquals(0, resultDto3.getCount());
+        resultDto2.getResults().forEach(levelDto -> {
+            assertEquals(200, levelDto.getMinBpm(),
+                    "min bpm should be equal to 200");
+            assertEquals(200, levelDto.getMaxBpm(),
+                    "max bpm should be equal to 200");
+        });
+    }
+
 
 }
