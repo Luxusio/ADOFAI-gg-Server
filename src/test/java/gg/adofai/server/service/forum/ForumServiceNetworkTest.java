b25c4d2f339dc78b73255f06729c35f0a56ba9bc
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-08-01 04:00:27 +0900

    Chore: Flush EntityManager before insert some entities

--- src/test/java/gg/adofai/server/service/forum/ForumServiceNetworkTest.java
+++ src/test/java/gg/adofai/server/service/forum/ForumServiceNetworkTest.java
@@ -2,8 +2,10 @@ package gg.adofai.server.service.forum;
 
 import gg.adofai.server.service.forum.dto.ForumTagDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import javax.persistence.EntityManager;
 import java.io.IOException;
 import java.util.List;
 
@@ -16,7 +18,7 @@ class ForumServiceNetworkTest {
     void testGetData() throws IOException {
         // given
         ForumService forumService = new ForumService(null, null,
-                null, null, null, null, null, WebClient.builder());
+                null, null, null, null, null, null, WebClient.builder());
 
         // when
         List<ForumTagDto> forumTagDtoList = forumService.getData(ForumService.GID_ADMIN_TAG, ForumTagDto::createForumTagDto).block();
